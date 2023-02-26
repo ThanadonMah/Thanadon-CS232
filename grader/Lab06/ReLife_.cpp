25cbac19a83c34af05760f0e10973757f393b4e0
Pun Thanadon <Thanadon.mah@ku.th>
2023-02-26 20:40:45 +0700

    Updated

--- grader/Lab06/ReLife_.cpp
+++ grader/Lab06/ReLife_.cpp
@@ -5,44 +5,70 @@
 
 using namespace std;
 
-int main()
-{
-    map<char, int> fre;
-    map<char, bool> check;
-    map<char, string> code;
-    vector<pair<int, string> > v;
-    string line, unique="";
-    getline(cin, line);
+map<char, int> frequent;
+map<char, bool> check;
+map<char, string> output;
+vector<pair<int, string> > v;
+string line, unique="";
 
-    for(int i=0; i<line.length(); i++) {
+void toUpper()
+{
+    for(int i=0 ; i<line.length() ; i++) {
         char c = toupper(line[i]);
-        fre[c]++;
+        frequent[c]++;
         if(check[c] == false){
             check[c] = true;
             unique = unique + c;
         }
     }
+}
 
+void count()
+{
     for(int i=0;i<unique.length();i++) {
         string temp = "";
         temp = temp + unique[i];
         vector<pair<int,string> >::iterator itr = v.begin();
-        while(itr!=v.end() && fre[unique[i]] >= itr->first) itr++;
-        v.insert(itr,make_pair(fre[unique[i]],temp));
+        while(itr!=v.end() && frequent[unique[i]] >= itr->first) itr++;
+        v.insert(itr,make_pair(frequent[unique[i]],temp));
     }
+}
 
-    while(v.size()>1)
+void encode()
+{
+    while(v.size() > 1)
     {
-        vector<pair<int,string> >::iterator l=v.begin(), r=v.begin()+1;
-        for(int i=0;i<l->second.length();i++) code[l->second[i]] = "0" + code[l->second[i]];
-        for(int i=0;i<r->second.length();i++) code[r->second[i]] = "1" + code[r->second[i]];
-        pair<int,string> p = make_pair(l->first+r->first,l->second+r->second);
-        v.erase(l,r+1);
+        vector<pair<int,string> >::iterator left=v.begin(), right=v.begin()+1;
+        for(int i=0;i<left->second.length();i++) 
+        {
+            output[left->second[i]] = "0" + output[left->second[i]];
+        }
+        for(int i=0;i<right->second.length();i++) 
+        {
+            output[right->second[i]] = "1" + output[right->second[i]];
+        }
+        pair<int,string> p = make_pair(left->first+right->first,left->second+right->second);
+        v.erase(left,right+1);
 
         vector<pair<int,string> >::iterator itr=v.begin();
         while(itr!=v.end() && p.first > itr->first) itr++;
         v.insert(itr,p);
     }
+}
 
-    for(int i=0;i<line.length();i++) cout << code[toupper(line[i])];
+void displayCode()
+{
+    for(int i=0 ; i<line.length() ; i++) 
+    {
+        cout << output[toupper(line[i])];
+    }
+}
+
+int main()
+{
+    getline(cin, line);
+    toUpper();
+    count();
+    encode();
+    displayCode();
 }
