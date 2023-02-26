de7880797bdedd3a3855f54f386ce8b55ac26ff8
Pun Thanadon <Thanadon.mah@ku.th>
2023-02-26 20:49:48 +0700

    Updated

--- grader/Lab06/SplitOffForest.cpp
+++ grader/Lab06/SplitOffForest.cpp
@@ -4,33 +4,21 @@
 
 using namespace std;
 
-int parent(int *arr,int x){
+int n,e,src;
+int ans=0;
+vector<pair<int,pair<int,int> > > g;
+
+int parent(int *arr,int x)
+{
     return (arr[x]==x)? x : arr[x] = parent(arr,arr[x]);
 }
 
-void merge(int *arr,int x,int y){
+void merge(int *arr,int x,int y)
+{
     arr[parent(arr,y)] = parent(arr,x);
 }
 
-int main()
-{
-    int n,e,src;
-    int ans=0;
-    cin >> n >> e;
-    if(n<1 || e<0) 
-    {
-        cout << "What did you expect?";
-        return 0; 
-    }
-    cin >> src;
-    if(src<1 || src>n) 
-    {
-        cout << "What did you expect?";
-        return 0; 
-    }
-
-    vector<pair<int,pair<int,int> > > g;
-
+bool createNode(){
     for(int i=0;i<e;i++)
     {
         int u,v,w;
@@ -38,7 +26,7 @@ int main()
         if(u<1 || u>n || v<1 || v>n || w<0) 
         {
             cout << "What did you expect?";
-            return 0; 
+            return false;
         }
         if(u==v) 
         {
@@ -47,10 +35,16 @@ int main()
         g.push_back(make_pair(w,make_pair(u,v)));
     }
     sort(g.begin(),g.end());
+    return true;
+}
 
+void traverse()
+{
     int arr[n+1];
-    for(int i=0;i<=n;i++) arr[i] = i;
-
+    for(int i=0;i<=n;i++) 
+    {
+        arr[i] = i;
+    }
     for(int i=0;i<g.size();i++)
     {
         pair<int,int> p = g[i].second;
@@ -61,5 +55,26 @@ int main()
         ans += g[i].first;
         merge(arr,p.first,p.second);
     }
-    cout << ans;
+}
+
+int main()
+{
+    
+    cin >> n >> e;
+    if(n<1 || e<0) 
+    {
+        cout << "What did you expect?";
+        return 0;
+    }
+    cin >> src;
+    if(src<1 || src>n) 
+    {
+        cout << "What did you expect?";
+        return 0;
+    }
+    if(createNode())
+    {
+        traverse();
+        cout << ans;
+    }
 }
